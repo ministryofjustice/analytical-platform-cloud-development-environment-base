883610583125d211766a3f260314f88331255baa
Gary H <26419401+Gary-H9@users.noreply.github.com>
2024-11-18 16:28:06 +0000

    :wrench: Update to also copy files

--- src/opt/analytical-platform/init/40-jupyter-kernel.sh
+++ src/opt/analytical-platform/init/40-jupyter-kernel.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-# Restore default Bash configuration if it doesn't exist
-
 echo "40-jupyter-kernel.sh"
 
-# Define the base directory to search
-BASE_DIR="/home/jovyan/.local/share/jupyter/kernels"
+SOURCE_DIR="/home/jovyan/.local/share/jupyter/kernels"
+TARGET_DIR="/home/analyticalplatform/.local"
+BASE_DIR="$TARGET_DIR/share/jupyter/kernels"
+
+# Copy .local directory to /home/analyticalplatform/.local
+if [ -d "$SOURCE_DIR" ]; then
+  echo "Copying $SOURCE_DIR to $TARGET_DIR..."
+  cp -r "$SOURCE_DIR"/* "$TARGET_DIR" || { echo "Copy failed."; exit 1; }
+  echo "Copy completed."
+else
+  echo "Source directory $SOURCE_DIR does not exist."
+  exit 1
+fi
 
 # Find all kernel.json files and replace 'jovyan' with 'analyticalplatform'
 if [ -d "$BASE_DIR" ]; then
+  echo "Searching for kernel.json files in $BASE_DIR..."
   if find "$BASE_DIR" -name "kernel.json" | grep -q 'kernel.json'; then
+    echo "Found kernel.json files. Editing..."
     find "$BASE_DIR" -name "kernel.json" -exec sed -i '' 's/jovyan/analyticalplatform/g' {} +
+    echo "Editing completed."
   else
-    echo "No kernel.json files found in $BASE_DIR"
+    echo "No kernel.json files found in $BASE_DIR."
   fi
 else
-  echo "Base directory $BASE_DIR does not exist"
+  echo "Base directory $BASE_DIR does not exist."
+  exit 1
 fi
